fb9831079f2392324909b7c0d4f45f767349091a
108-0 <jchangjun6@gmail.com>
2024-09-12 16:06:26 +0900

    9/12 roi와 colorspace conversion

--- 04letters.py
+++ 04letters.py
@@ -1,19 +1,32 @@
+from turtledemo.nim import COLOR
+
 import cv2
 import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+def korean(src, text, pos, font_size, font_color):
+    img_pil = Image.fromarray(src)
+    draw = ImageDraw.Draw(img_pil)
+    font = ImageFont.truetype('fonts/gulim.ttc', font_size)
+    draw.text(pos, text, font=font, fill=font_color)
+    return np.array(img_pil)
+
 img = np.full((500,500,3), 255, dtype=np.uint8)
 
-cv2.putText(img, "Plain", (50,30), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0))
-cv2.putText(img, "Simplex", (50,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0))
-cv2.putText(img, "Duplex", (50, 110), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,0))
-cv2.putText(img, "Simplex X 2", (200,110), cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,250))
-cv2.putText(img, "Complex", (50,200), cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,0,0))
-cv2.putText(img, "Complex", (50,260), cv2.FONT_HERSHEY_TRIPLEX,1,(0,0,0))
-cv2.putText(img, "Complex", (200,260), cv2.FONT_HERSHEY_TRIPLEX ,1,(0,0,0))
-cv2.putText(img, "Complex", (200,260), cv2.FONT_HERSHEY_COMPLEX_SMALL,2,(0,0,255))
-cv2.putText(img, "아름다운 강산 -이용희", (50,470), cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,0,0))
+# cv2.putText(img, "Plain", (50,30), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0))
+# cv2.putText(img, "Simplex", (50,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0))
+# cv2.putText(img, "Duplex", (50, 110), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,0))
+# cv2.putText(img, "Simplex X 2", (200,110), cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,250))
+# cv2.putText(img, "Complex", (50,200), cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,0,0))
+# cv2.putText(img, "Complex", (50,260), cv2.FONT_HERSHEY_TRIPLEX,1,(0,0,0))
+# cv2.putText(img, "Complex", (200,260), cv2.FONT_HERSHEY_TRIPLEX ,1,(0,0,0))
+# cv2.putText(img, "Complex", (200,260), cv2.FONT_HERSHEY_COMPLEX_SMALL,2,(0,0,255))
+
+COLOR = (0,0,0)
+FONT_SIZE = 30
+
+img = korean(img, "아름다운 강산 -이용희", (20,50), FONT_SIZE, COLOR)
 
 
 cv2.imshow('letters', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-//
---
+++ 07ColorSpaceConversion.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+img_file = "./img/girl.jpg"
+img = cv2.imread(img_file)
+print(img.shape)
+
+cv2.imshow("IMG", img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+imgy = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+y, u, v = cv2.split(imgy)
+
+cv2.imshow("IMG2", y)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+imgyy = np.full((293,406),255, dtype=np.uint8)
+b, g, r = cv2.split(img)
+imgyy = 0.299*r + 0.587*g +0.114*b
+imgyy = imgyy.astype(np.uint8)
+diff = y - imgyy
+cv2.imshow("IMG", img)
+cv2.imshow("IMG cv2변환", y)
+cv2.imshow("IMG 계산변환", imgyy)
+cv2.imshow("diffrent", diff)
+cv2.waitKey()
+cv2.destroyAllWindows()
